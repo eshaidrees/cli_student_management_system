7ca0f08181cf2ff33fdfb2e9e4fd500d77b6bc7b
eshaidrees <eshaidrees190@gmail.com>
2024-05-12 11:56:48 -0700

    student management system

---
+++ index.ts
@@ -0,0 +1,138 @@
+#! /usr/bin/env node
+
+ import inquirer from "inquirer";
+ import chalk from "chalk";
+class student {
+    Name:string;
+    StudID:string;
+    TuitionFee: number;
+    EnrollCourse: string[]
+    constructor(Name:string,StudID:string,TuitionFee:number,EnrollCourse:string[]){
+        this.Name = Name
+        this.StudID = StudID
+        this.TuitionFee = TuitionFee
+        this.EnrollCourse = EnrollCourse
+    }
+  }
+let baseID = 12345
+let studentID: string = "";
+let enrollment = true;
+
+let students: student[] = []
+
+do{
+  let  answer = await inquirer.prompt({
+    type: "list",
+    name: "ans",
+    message: "Select an option\n",
+    choices: ["Enroll a student" , "Show student status"]
+  })
+   
+  if(answer.ans === "Enroll a student" ){
+    let studentName = await inquirer.prompt({
+      type: "input",
+      name: "ans",
+      message: "Enter your name:"
+    })
+
+    let trimStudentName = (studentName.ans).trim().toLowerCase()
+    let studentNameCheck = students.map(obj => obj.Name)
+
+    if(studentNameCheck.includes(trimStudentName) === false){
+      if (trimStudentName !== ""){
+        baseID++
+        studentID = "STUD" + baseID
+        console.log(chalk.yellow(`Welcome, ${trimStudentName}!`));
+        
+        let course = await inquirer.prompt({
+          type: "list",
+          name:"ans",
+          message: "Select a Course",
+          choices: [ 'JavaScript',  'Python', 'Data Science', 'Machine Learning']
+        })
+        let courseFee = 0;
+        switch(course.ans){
+          case "JavaScript":
+            courseFee = 5000;
+            break;
+          
+          case "Python":
+             courseFee = 4000;
+             break;
+  
+          case "Data Science":
+            courseFee = 3000;
+            break;
+    
+          case "Machine Learning":
+             courseFee = 2000;
+             break;
+
+        }
+
+        let courseConfirm = await inquirer.prompt({
+          type: "confirm",
+          name: "ans",
+          message: "Do you want to enroll in this course"
+        })
+
+        if(courseConfirm.ans === true){
+            let Student = new student(trimStudentName,studentID,courseFee,[course.ans])
+            students.push(Student)
+
+            console.log(chalk.yellow("You have enrolled in this course"));
+            
+
+        }
+  
+      }else{
+        console.log(chalk.red("invaled Name"));
+      }
+    }else{
+      console.log(chalk.red("this name is already exist"));
+    }
+    
+  }
+  else if(answer.ans ===  "Show student status" ){
+    if(students.length !== 0){
+      let studentNamesCheck = students.map(e => e.Name)
+
+      let selecStudent  = await inquirer.prompt({
+        type: "list",
+        name: "ans",
+        message: "Select name",
+        choices: studentNamesCheck
+      })
+
+      let foundStudent = students.find(Student => Student.Name === selecStudent.ans)
+
+      console.log(chalk.yellow("\nStudent Information"));
+      console.log(foundStudent);
+      console.log("\n");
+      
+      
+      
+    }else{
+      console.log(chalk.red("Record is empty"));
+      
+    }
+
+    let userConfirm  = await inquirer.prompt({
+      type: "confirm",
+      name: "ans",
+      message: "Do you want to continue?"
+    })    
+
+    if(userConfirm.ans === false){
+      enrollment = false
+    }
+  }
+} while(enrollment)
+
+  console.log(chalk.yellow("\n\tExit Student Management System"));
+
+
+
+
+
+  
